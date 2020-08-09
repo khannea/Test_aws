4d5475ca2ec7b7710f0aa269ad5651bc379f23ad
unknown <khanhigoufake2@gmail.com>
2020-08-09 22:43:49 +0200

    line

--- app.js
+++ app.js
@@ -7,4 +7,6 @@ app.get('/', (req, res) => {
 
 const port = process.env.port || 3000;
 
-app.listen(port, () => console.log('WAZAAAAAAAAAAaa'));
+app.listen(port, () => {
+  console.log('WAZAAAAAAAAAAaa');
+});
