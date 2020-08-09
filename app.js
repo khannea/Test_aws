caf9b92aeb200d85ce5a077d96ea083371846fc5
unknown <khanhigoufake2@gmail.com>
2020-08-09 22:22:20 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome homepage Baby!!');
+});
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => console.log('WAZAAAAAAAAAAaa'));
